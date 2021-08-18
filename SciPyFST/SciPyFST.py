73aae6a9ea9f7917b815aad33737b6a251d113e4
MorriganR <dasha@ddyka.com>
2021-08-18 11:48:37 +0200

    fix comment style, etc

--- SciPyFST/SciPyFST.py
+++ SciPyFST/SciPyFST.py
@@ -1,32 +1,45 @@
 class SciPyFST:
     def __init__(self, states, initState, inAlphabet, outAlphabet, transitionFunction, outputFunction):
-        # states = [0,1,2]
         self.states = states
+        """ states = [0,1,2] """
 
-        # initState = 0
         self.initState = initState
+        """ initState = 0 """
 
-        # inAlphabet = [0,1]
         self.inAlphabet = inAlphabet
+        """ inAlphabet = [0,1] """
 
-        # outAlphabet = [0,1,2]
         self.outAlphabet = outAlphabet
+        """ outAlphabet = [0,1,2] """
 
-        # transitionFunction [ [State, inAlphabet, nextState], ...]
-        # transitionFunction = [ [0,0,1], [1,0,1], [1,1,2] ]
         self.transitionFunction = transitionFunction
+        """ transitionFunction [ [State, inAlphabet, nextState], ...]\n
+        transitionFunction = [ [0,0,1], [1,0,1], [1,1,2] ] """
 
-        # outputFunction Moore [ [State, outAlphabet], ...]
-        # outputFunction = [ [0,0], [1,0], [2,2]]
-        # outputFunction Mealy [ [State, inAlphabet, outAlphabet], ...]
-        # outputFunction = [ [0,1,0], [1,1,0], [2,2]]
         self.outputFunction = outputFunction
+        """
+        outputFunction Moore [ [State, outAlphabet], ...]\n
+        outputFunction = [ [0,0], [1,0], [2,2]]\n
+        outputFunction Mealy [ [State, inAlphabet, outAlphabet], ...]\n
+        outputFunction = [ [0,1,0], [1,1,0], [2,2,2]]
+        """
+
         self.type = self.__detTypeByOutputFunction()
 
     def __detTypeByOutputFunction(self):
-        return 'Moore or Mealy'
+        if len(self.outputFunction[0]) == 2:
+            return 'Moore'
+        return 'Mealy'
 
     def isValid(self):
-        if self.initState in self.states:
-            return True
-        return False
+        """ Check """
+        if self.initState not in self.states:
+            return False
+        # TODO more checks needed
+        return True
+
+    def toDot(self):
+        if self.initState not in self.states:
+            return False
+        # TODO more checks needed
+        return True
--- tests/test_init_FST.py
+++ tests/test_init_FST.py
@@ -15,13 +15,21 @@ class TestInitFST(unittest.TestCase):
         outAlphabet = [0,1,2]
         # transitionFunction [ [State, inAlphabet, nextState], ...]
         transitionFunction = [ [0,0,1], [1,0,1], [1,1,2] ]
+
         # outputFunction Moore [ [State, outAlphabet], ...]
-        outputFunction = [ [0,0], [1,0], [2,2]]
+        outputFunctionMoore = [ [0,0], [1,0], [2,2]]
         # outputFunction Mealy [ [State, inAlphabet, outAlphabet], ...]
-        # outputFunction = [ [0,1,0], [1,1,0], [2,2]]
-        zero_fst = FST(states, initState, inAlphabet, outAlphabet, transitionFunction, outputFunction)
-        self.assertTrue(zero_fst.isValid())
-        zero2_fst = FST(states, 5, inAlphabet, outAlphabet, transitionFunction, outputFunction)
+        outputFunctionMealy = [ [0,1,0], [1,1,0], [2,2,2]]
+
+        fstMoore = FST(states, initState, inAlphabet, outAlphabet, transitionFunction, outputFunctionMoore)
+        self.assertTrue(fstMoore.isValid())
+        self.assertEqual(fstMoore.type, 'Moore')
+
+        fstMealy = FST(states, initState, inAlphabet, outAlphabet, transitionFunction, outputFunctionMealy)
+        self.assertTrue(fstMealy.isValid())
+        self.assertEqual(fstMealy.type, 'Mealy')
+
+        zero2_fst = FST(states, 5, inAlphabet, outAlphabet, transitionFunction, outputFunctionMoore)
         self.assertFalse(zero2_fst.isValid())
 
 if __name__ == '__main__':
