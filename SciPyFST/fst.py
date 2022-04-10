2b454a83c7e81d461a2fd978d9170a3cc6c06004
MorriganR <dasha@ddyka.com>
2022-04-10 12:01:24 -0700

    fst.py: making flake8 happy

--- SciPyFST/fst.py
+++ SciPyFST/fst.py
@@ -1,25 +1,26 @@
 from copy import deepcopy
 
+
 class fst:
     def __init__(self,
-                states:list=[],
-                initState=None,
-                inAlphabet:list=[],
-                outAlphabet:list=[],
-                transitionFunction:list=[],
-                outputFunction:list=[],
-                finalStates:list=[],
-        ):
-        self.states = list(dict.fromkeys(states))#, key=str)
+                 states: list = [],
+                 initState=None,
+                 inAlphabet: list = [],
+                 outAlphabet: list = [],
+                 transitionFunction: list = [],
+                 outputFunction: list = [],
+                 finalStates: list = [], ):
+
+        self.states = list(dict.fromkeys(states))
         """ states = [0,1,2] """
 
         self.initState = deepcopy(initState)
         """ initState = 0 """
 
-        self.inAlphabet = list(dict.fromkeys(inAlphabet))#, key=str)
+        self.inAlphabet = list(dict.fromkeys(inAlphabet))
         """ inAlphabet = [0,1] """
 
-        self.outAlphabet = list(dict.fromkeys(outAlphabet))#, key=str)
+        self.outAlphabet = list(dict.fromkeys(outAlphabet))
         """ outAlphabet = [0,1,2] """
 
         self.transitionFunction = deepcopy(transitionFunction)
@@ -34,14 +35,14 @@ class fst:
         outputFunction = [ [0,1,0], [1,1,0], [2,2,2]]
         """
 
-        self.finalStates = list(dict.fromkeys(finalStates))#, key=str)
+        self.finalStates = list(dict.fromkeys(finalStates))
         """ finalStates = [0,1,2] """
 
         self.__type = self.__detTypeByOutputFunction()
 
-        self.states = list(dict.fromkeys(self.states + self.__getStatesFromTransitionAndOutputFunction()))#, key=str)
-        self.inAlphabet = list(dict.fromkeys(self.inAlphabet + self.__getInAlphabetFromTransitionAndOutputFunction()))#, key=str)
-        self.outAlphabet = list(dict.fromkeys(self.outAlphabet + self.__getOutAlphabetFromTransitionAndOutputFunction()))#, key=str)
+        self.states = list(dict.fromkeys(self.states + self.__getStatesFromTransitionAndOutputFunction()))
+        self.inAlphabet = list(dict.fromkeys(self.inAlphabet + self.__getInAlphabetFromTransitionAndOutputFunction()))
+        self.outAlphabet = list(dict.fromkeys(self.outAlphabet + self.__getOutAlphabetFromTransitionAndOutputFunction()))
 
         self.trFuncDict = dict()
         for (curentState, inSignal, nextState) in self.transitionFunction:
@@ -76,7 +77,7 @@ class fst:
         return 'FSM'
 
     def __getStatesFromTransitionAndOutputFunction(self):
-        #if self.isFSM():
+        # if self.isFSM():
         #    return []
         toOut = []
         if self.initState is not None:
@@ -94,7 +95,7 @@ class fst:
             for (curentState, inSignal, outSignal) in self.outputFunction:
                 if curentState is not None:
                     toOut.append(curentState)
-        return list(dict.fromkeys(toOut))#, key=str)
+        return list(dict.fromkeys(toOut))
 
     def __getInAlphabetFromTransitionAndOutputFunction(self):
         toOut = []
@@ -105,7 +106,7 @@ class fst:
             for (curentState, inSignal, outSignal) in self.outputFunction:
                 if inSignal is not None:
                     toOut.append(inSignal)
-        return list(dict.fromkeys(toOut))#, key=str)
+        return list(dict.fromkeys(toOut))
 
     def __getOutAlphabetFromTransitionAndOutputFunction(self):
         if self.isFSM():
@@ -119,7 +120,7 @@ class fst:
             for (curentState, inSignal, outSignal) in self.outputFunction:
                 if outSignal is not None:
                     toOut.append(outSignal)
-        return list(dict.fromkeys(toOut))#, key=str)
+        return list(dict.fromkeys(toOut))
 
     def isValid(self):
         """ Check TODO more checks needed """
@@ -154,15 +155,15 @@ class fst:
         return False
 
     def deepcopy(self):
-        fstOut = fst(self.states, self.initState, self.inAlphabet,\
-            self.outAlphabet, self.transitionFunction, self.outputFunction,\
-            self.finalStates)
+        fstOut = fst(self.states, self.initState, self.inAlphabet,
+                     self.outAlphabet, self.transitionFunction, self.outputFunction,
+                     self.finalStates)
         return fstOut
 
     def addState(self, state):
         if state not in self.states:
             self.states.append(state)
-            self.states = list(dict.fromkeys(self.states))#, key=str)
+            self.states = list(dict.fromkeys(self.states))
         return True
 
     def addTransition(self, curentState, inSignal, nextState):
@@ -212,14 +213,14 @@ class fst:
         ε-closure is NOT INCLUDED!
         if input signal == None return set of ε-accessible in one step states
         """
-        states = set(curentStates) if isinstance(curentStates, (list, set)) else set([curentStates,])
+        states = set(curentStates) if isinstance(curentStates, (list, set)) else set([curentStates, ])
         nextStates = set()
         for state in states:
             if (state, inSignal) in self.trFuncDict:
                 temp = self.trFuncDict.get((state, inSignal))
-                nextStates.update( temp if isinstance(temp, (list, set)) else set([temp,]) )
+                nextStates.update(temp if isinstance(temp, (list, set)) else set([temp, ]))
             elif inSignal is not None:
-                nextStates.update( [None,] )
+                nextStates.update([None, ])
         return nextStates
 
     def getOutSignal(self, curentState, inSignal, ifNotInDict=None):
@@ -238,7 +239,7 @@ class fst:
         for inSignal in inSignals:
             outStates.append(curentState)
             outSignals.append(self.getOutSignal(curentState, inSignal, -1))
-            curentState = self.getNextState(curentState, inSignal)#, curentState)
+            curentState = self.getNextState(curentState, inSignal)  # , curentState)
         outStates.append(curentState)
         if self.isMoore():
             outSignals.append(self.getOutSignal(curentState, inSignal, -1))
@@ -250,7 +251,7 @@ class fst:
         return ε-closure of a state or set of states
         https://en.wikipedia.org/wiki/Nondeterministic_finite_automaton#%CE%B5-closure_of_a_state_or_set_of_states
         """
-        inStates = set(states) if isinstance(states, (list, set)) else set([states,])
+        inStates = set(states) if isinstance(states, (list, set)) else set([states, ])
         while True:
             nextStates = self.getNextStates(inStates, None)
             if set(nextStates).issubset(inStates):
@@ -260,23 +261,24 @@ class fst:
 
     def playFSM(self, inSignals: list, debug=None):
         def __debug(a):
-            if debug: print("--> " + a)
-        __debug( "Start print debug info for playFSM():" )
+            if debug:
+                print("--> " + a)
+        __debug("Start print debug info for playFSM():")
         curentStates = set(self.initState) \
             if isinstance(self.initState, (list, set)) \
-            else set([self.initState,])
+            else set([self.initState, ])
         curentStates = self.getEpsilonClosure(curentStates)
         for inSignal in inSignals:
-            __debug( "  curent state(s): " + str(curentStates) )
-            __debug( "  input signal: " + str(inSignal) )
+            __debug("  curent state(s): " + str(curentStates))
+            __debug("  input signal: " + str(inSignal))
             nextStates = self.getNextStates(curentStates, inSignal)
-            __debug( "    next state(s): " + str(nextStates) )
+            __debug("    next state(s): " + str(nextStates))
             curentStates = self.getEpsilonClosure(nextStates)
-            __debug( "      + ε-closure: " + str(curentStates) )
+            __debug("      + ε-closure: " + str(curentStates))
         if curentStates & set(self.finalStates):
-            __debug( "accepting state(s): " + str(curentStates & set(self.finalStates)) )
+            __debug("accepting state(s): " + str(curentStates & set(self.finalStates)))
             return True
-        __debug( "last states: " + str( curentStates ) + ", all accepting states: " + str(self.finalStates) )
+        __debug("last states: " + str(curentStates) + ", all accepting states: " + str(self.finalStates))
         return False
 
     def asMoore(self):
@@ -284,32 +286,33 @@ class fst:
             return self.deepcopy()
 
         initStateMoore = 0
-        #initStateSignalMoore = -1
-        newMooreState_qi = initStateMoore # q0 without q
-        markedTranOut = dict() # key - [topHeaderState_Si, leftHeaderSignal_Xj], val - existMooreState_qi. See point (1)
-        dictForSearchNewState = dict() # key - [tableState_Sm, tableSignal_Yn] from table, val - existMooreState_qi. See point (1)
-        eqStatesFor_Si = dict() # key - tableState_Sm, val - list of existMooreState_qi [0, 2, ...]. See point (2)
+        # initStateSignalMoore = -1
+        newMooreState_qi = initStateMoore  # q0 without q
+        markedTranOut = dict()  # key - [topHeaderState_Si, leftHeaderSignal_Xj], val - existMooreState_qi. See point (1)
+        dictForSearchNewState = dict()  # key - [tableState_Sm, tableSignal_Yn] from table, val - existMooreState_qi. See point (1)
+        eqStatesFor_Si = dict()  # key - tableState_Sm, val - list of existMooreState_qi [0, 2, ...]. See point (2)
 
-        outputFunctionMoore = [] # list [[state, outSignal], [...] ... ]. See point (3)
+        outputFunctionMoore = []  # list [[state, outSignal], [...] ... ]. See point (3)
 
         # add q0 state to S0
-        #mapForSearchNewState[self.initState, None] = newMooreState_qi
+        # mapForSearchNewState[self.initState, None] = newMooreState_qi
         eqStatesFor_Si[self.initState] = [newMooreState_qi]
-        #outputFunctionMoore.append([newMooreState_qi, initStateSignalMoore])
+        # outputFunctionMoore.append([newMooreState_qi, initStateSignalMoore])
 
         # mark all equivalent transition-output pair as new Moore states. See point (1) & (2)
         for topHeaderState_Si in self.states:
             for leftHeaderSignal_Xj in self.inAlphabet:
                 tableState_Sm = self.getNextState(topHeaderState_Si, leftHeaderSignal_Xj)
-                if tableState_Sm is None: continue
+                if tableState_Sm is None:
+                    continue
                 tableSignal_Yn = self.getOutSignal(topHeaderState_Si, leftHeaderSignal_Xj)
-                existMooreState_qi = dictForSearchNewState.get((tableState_Sm, tableSignal_Yn)) # (1)
+                existMooreState_qi = dictForSearchNewState.get((tableState_Sm, tableSignal_Yn))  # (1)
                 if existMooreState_qi is None:
                     newMooreState_qi += 1
                     existMooreState_qi = newMooreState_qi
-                    dictForSearchNewState[(tableState_Sm, tableSignal_Yn)] = existMooreState_qi # (1) add new state
+                    dictForSearchNewState[(tableState_Sm, tableSignal_Yn)] = existMooreState_qi  # (1) add new state
                     outputFunctionMoore.append([existMooreState_qi, tableSignal_Yn])
-                    tempList = eqStatesFor_Si.get(tableState_Sm, []) # (2)
+                    tempList = eqStatesFor_Si.get(tableState_Sm, [])  # (2)
                     tempList.append(newMooreState_qi)
                     eqStatesFor_Si[tableState_Sm] = tempList
                 markedTranOut[topHeaderState_Si, leftHeaderSignal_Xj] = existMooreState_qi
@@ -329,9 +332,9 @@ class fst:
         Qstack_dfa = list()
         Qvisited_dfa = set()
         T_dfa = list()
-        q0_dfa = self.getEpsilonClosure(set(self.initState) \
-                if isinstance(self.initState, (list, set, frozenset)) \
-                else set([self.initState,]))
+        q0_dfa = self.getEpsilonClosure(
+            set(self.initState) if isinstance(self.initState, (list, set, frozenset))
+            else set([self.initState, ]))
         q0_dfa.discard(None)
         q0_dfa = tuple([c for b, c in sorted([(str(a), a) for a in q0_dfa])])
         Qstack_dfa.append(q0_dfa)
@@ -355,11 +358,12 @@ class fst:
                 if state_n in self.finalStates:
                     finalQ_dfa.append(state_d)
                     continue
-        return fst(initState = q0_dfa, transitionFunction = T_dfa, finalStates = finalQ_dfa)
+        return fst(initState=q0_dfa, transitionFunction=T_dfa, finalStates=finalQ_dfa)
 
     def getTestSignal(self):
         listOfInSignalsList = []
-        def recGetNext(curentState, visitedStates:dict, inSignals:list):
+
+        def recGetNext(curentState, visitedStates: dict, inSignals: list):
             if visitedStates.get(curentState) is None:
                 visitedStates[curentState] = 1
                 for inSignal in self.inAlphabet:
@@ -377,15 +381,15 @@ class fst:
         recGetNext(self.initState, dict(), [])
         return listOfInSignalsList
 
-    def _isContains(self, fst:'fst'):
+    def _isContains(self, fst: 'fst'):
         listOfInSignalsList = fst.getTestSignal()
         for inSignalList in listOfInSignalsList:
             if self.playFST(inSignalList)[0] != fst.playFST(inSignalList)[0]:
                 return False
         return True
 
-    def isContains(self, fst:'fst'):
-        def recGetNext(curentState, visitedStates:dict, inSignals:list):
+    def isContains(self, fst: 'fst'):
+        def recGetNext(curentState, visitedStates: dict, inSignals: list):
             if visitedStates.get(curentState) is None:
                 visitedStates[curentState] = 1
                 for inSignal in fst.inAlphabet:
@@ -402,12 +406,13 @@ class fst:
             return True
         return recGetNext(fst.initState, dict(), [])
 
-    def isSimilar(self, fst:'fst'):
+    def isSimilar(self, fst: 'fst'):
         return self.isContains(fst) and fst.isContains(self)
 
     def getUnreachableStates(self):
         unreachableStates = dict.fromkeys(self.states)
-        def recGetNext(curentState, visitedStates:dict):
+
+        def recGetNext(curentState, visitedStates: dict):
             unreachableStates.pop(curentState, None)
             if visitedStates.get(curentState) is None:
                 visitedStates[curentState] = 1
@@ -419,5 +424,6 @@ class fst:
                         recGetNext(nextCurentState, deepcopy(visitedStates))
             else:
                 return
+
         recGetNext(self.initState, dict())
         return list(unreachableStates)
